b2c209c96657311bcce77ad5848c2e28ebdfba32
anonymous928392 <brayan.ramirez1@udea.edu.co>
2025-10-19 21:28:28 -0500

    main screen linked to other screens

--- app/src/main/java/co/edu/udea/compumovil/gr01_20252_lab2/MainActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr01_20252_lab2/MainActivity.kt
@@ -5,43 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import co.edu.udea.compumovil.gr01_20252_lab2.ui.theme.Labs2CM20252Gr01Theme
+import co.edu.udea.compumovil.gr01_20252_lab2.ui.navigation.NewsNavGraph
+import co.edu.udea.compumovil.gr01_20252_lab2.ui.theme.JetNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Labs2CM20252Gr01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            JetNewsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NewsNavGraph()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Labs2CM20252Gr01Theme {
-        Greeting("Android")
     }
 }
